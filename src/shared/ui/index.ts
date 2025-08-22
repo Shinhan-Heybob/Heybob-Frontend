ce8c695d560ed6e45eb3e81f888e821962bee599
jayeunpark <jayeunpark0704@gmail.com>
2025-08-22 14:59:32 +0900

    feat: Avatar Selector 컴포넌트 생성

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -3,7 +3,9 @@ export { Button } from './atoms/Button';
 export { Input } from './atoms/Input';
 export { Text } from './atoms/Text';
 export { Dropdown } from './atoms/Dropdown';
+export { CheckBox } from './atoms/CheckBox';
 
 // Molecules
 export { InputField } from './molecules/InputField';
 export { FormCard } from './molecules/FormCard';
+export { AvatarSelector } from './molecules/AvatarSelector';

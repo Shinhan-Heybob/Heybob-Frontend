e644ec194e4070c36400de6718e09b65b82bb7d0
jayeunpark <jayeunpark0704@gmail.com>
2025-08-23 23:53:24 +0900

    chore: 콘솔로그 삭제

--- app/meals/create.tsx
+++ app/meals/create.tsx
@@ -2,6 +2,5 @@ import { MealCreateScreen } from '@/src/features/meal-create/ui/MealCreateScreen
 import React from 'react';
 
 export default function MealCreatePage() {
-  console.log("MealCreatePage 렌더링됨")
   return <MealCreateScreen />;
 }
